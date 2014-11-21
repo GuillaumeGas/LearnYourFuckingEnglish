3dc367d47d7a0d34bb6cf5310cccc4d5b28b2638
Guillaume Gas <guillaume@gas28.net>
2014-11-21 18:33:01 +0100

    Add php code for voc and irreg vb.

--- src/php/IrregularVerbs.class.php
+++ src/php/IrregularVerbs.class.php
@@ -28,11 +28,63 @@ class IrregularVerbs {
         }
     }
 
-    private function save_vb() { return ""; }
-    private function print_add_form() { return ""; }
+    private function save_vb() {
+        for($i = 0; $i < 10; $i++) {
+            if(strlen($_POST['english_'.$i]) > 0) {
+                $query = $this->_bdd->prepare("INSERT INTO irreg_vb VALUES(NULL, :french, :english, :preterit, :perfect)");
+                $query->execute(array(":french" => $_POST['french_'.$i], ":english" => $_POST['english_'.$i], ":preterit" => $_POST['preterit_'.$i], ":perfect" => $_POST['perfect_'.$i]));
+            }
+        }
 
-    private function check_test() { return ""; }
-    private function show_test_form() { return ""; }
+        return $this->_smarty->fetch("html/add_vb.html");
+    }
+
+    private function print_add_form() {
+        return $this->_smarty->fetch("html/add_vb.html");
+    }
+
+    private function check_test() {
+        $nb_err = 0;
+        $err = "";
+        for($i = 0; $i < $_POST['nb_vb']; $i++) {
+            if($_POST['french_'.$i] != $_POST['correct_french_'.$i]
+                || $_POST['english_'.$i] != $_POST['correct_english_'.$i]
+                || $_POST['preterit_'.$i] != $_POST['correct_preterit_'.$i]
+                || $_POST['perfect_'.$i] != $_POST['correct_perfect_'.$i]) {
+                $nb_err++;
+                $err .= $_POST['correct_english_'.$i]." - ".$_POST['correct_preterit_'.$i]." - ".$_POST['correct_perfect_'.$i]." - ".$_POST['correct_french_'.$i]."<br>";
+            }
+        }
+
+        $this->_smarty->assign("Err", $nb_err);
+        $this->_smarty->assign("Lang", $_POST['lang']);
+        $this->_smarty->assign("ShowResult", "true");
+
+        return $this->_smarty->fetch("html/irr_verbs.html");
+    }
+
+    private function show_test_form() {
+        $lang = "french";
+        if(isset($_GET['lang'])) {
+            if($_GET['lang'] == "english") {
+                $lang = $_GET['lang'];
+            }
+        }
+
+        $query = $this->_bdd->query("SELECT * FROM irreg_vb ORDER BY RAND() LIMIT 10");
+        $voc = array();
+        $nb_voc = 0;
+        while($data = $query->fetch()) {
+            $voc[$nb_voc++] = $data;
+        }
+
+        $this->_smarty->assign("ListIrrVerbs", $voc);
+        $this->_smarty->assign("Lang", $lang);
+        $this->_smarty->assign("NbVerbs", $nb_voc);
+        $this->_smarty->assign("ShowResult", "false");
+
+        return $this->_smarty->fetch("html/irr_verbs.html");
+    }
 }
 
 ?>
--- src/php/Voc.class.php
+++ src/php/Voc.class.php
@@ -29,11 +29,135 @@ class Voc {
         }
     }
 
-    private function save_words() { return ""; }
-    private function print_add_form() { return ""; }
+    private function save_words() {
+        for($i = 0; $i < 10; $i++) {
+            if(strlen($_POST['english_'.$i]) > 0) {
+                $query = $this->_bdd->prepare("INSERT INTO voc VALUES(NULL, :english, :french)");
+                $query->execute(array(":english" => $_POST['english_'.$i], ":french" => $_POST['french_'.$i]));
+            }
+        }
 
-    private function check_test() { return ""; }
-    private function show_test_form() { return ""; }
+        return $this->_smarty->fetch("html/add_voc.html");
+    }
+    private function print_add_form() {
+        return $this->_smarty->fetch("html/add_voc.html");
+    }
+
+    private function check_test() {
+        $nb_err = 0;
+        $err = "";
+        for($i = 0; $i < $_POST['nb_vb']; $i++) {
+            if($this->word_already_read(0, $_POST['id_'.$i], $this->_bdd) == 0) {
+                $this->add_word_already_read(0, $_POST['id_'.$i], $this->_bdd);
+            }
+            if($_POST['translate_'.$i] != $_POST['correct_translate_'.$i]) {
+                $nb_err++;
+                $err .= $_POST['origin_'.$i]." != ".$_POST['translate_'.$i]." => ".$_POST['correct_translate_'.$i]."<br>";
+                $this->add_failure(0, $_POST['id_'.$i], $this->_bdd);
+            } else {
+                $this->add_success(0, $_POST['id_'.$i], $this->_bdd);
+            }
+        }
+
+        $this->_smarty->assign("Err", $err);
+        $this->_smarty->assign("NbErr", $nb_err);
+        $this->_smarty->assign("Lang", $_POST['lang']);
+        $this->_smarty->assign("ShowResult", "true");
+
+        return $this->_smarty->fetch("html/voc.html");
+    }
+
+    private function show_test_form() {
+        $lang = "french";
+        if(isset($_GET['lang'])) {
+            if($_GET['lang'] == "english") {
+                $lang = $_GET['lang'];
+            }
+        }
+
+        // 1 : prendre 5 nouveaux mots
+        // 2 : prendre 5 mots déjà connus (% de réussite > 80)
+        // compléter avec des mots pas bien connus
+
+        $query_1 = $this->_bdd->prepare("SELECT * FROM voc WHERE id NOT IN (SELECT id_word FROM voc_already_read WHERE id_user = :id_user) LIMIT 5");
+        $query_1->execute(array(":id_user" => 0));
+
+        $voc = array();
+        $nb_voc = 0;
+        while($data = $query_1->fetch()) {
+            $voc[$nb_voc] = $data;
+            $voc[$nb_voc++]['success'] = 0;
+        }
+
+        $query_2 = $this->_bdd->prepare("SELECT V.id, V.english, V.french, VAR.id_word, VAR.success, VAR.failures FROM voc V, voc_already_read VAR WHERE V.id = VAR.id_word AND VAR.id_user = 0 ORDER BY RAND()");
+        $query_2->execute(array(":id_user" => 0));
+
+        $tmp_voc = array();
+        $nb_already_read = 0;
+        while($data = $query_2->fetch()) {
+            $tmp_voc[$nb_already_read] = $data;
+            $tmp_voc[$nb_already_read]["success"] = ($data['success']/($data['success']+$data['failures']))*100;
+            $nb_already_read++;
+        }
+
+        $nb_word_already_know = 0;
+        $i = 0;
+        while($i < $nb_already_read && $nb_word_already_know < 5) {
+            if($tmp_voc[$i]['success'] >= 80) {
+                $voc[$nb_voc++] = $tmp_voc[$i];
+                $nb_word_already_know++;
+            }
+            $i++;
+        }
+
+        $i = 0;
+        while($nb_voc < 20 && $i < $nb_already_read) {
+            if($tmp_voc[$i]['success'] < 80) {
+                $voc[$nb_voc++] = $tmp_voc[$i];
+            }
+            $i++;
+        }
+
+        $this->_smarty->assign("ListVoc", $voc);
+        $this->_smarty->assign("Lang", $lang);
+        $this->_smarty->assign("NbVoc", $nb_voc);
+        $this->_smarty->assign("ShowResult", "false");
+
+        return $this->_smarty->fetch("html/voc.html");
+    }
+
+
+    private function select_word($id_user, $id_word, $bdd) {
+        $query = $bdd->prepare("SELECT * FROM voc_already_read WHERE id_word = :id_word AND id_user = :id_user");
+        $query->execute(array(":id_word" => $id_word, ":id_user" => $id_user));
+        return $query->fetch();
+    }
+
+    private function word_already_read($id_user, $id_word, $bdd) {
+        $query = $bdd->prepare("SELECT COUNT(*) AS nb FROM voc_already_read WHERE id_word = :id_word AND id_user = :id_user");
+        $query->execute(array(":id_word" => $id_word, ":id_user" => $id_user));
+        $nb = $query->fetch()['nb'];
+        return $nb;
+    }
+
+    private function add_word_already_read($id_user, $id_word, $bdd) {
+        $query = $bdd->prepare("INSERT INTO voc_already_read VALUES(:id_user, :id_word, 0, 0)");
+        $query->execute(array(":id_word" => $id_word, ":id_user" => $id_user));
+    }
+
+    private function add_failure($id_user, $id_word, $bdd) {
+        $data_word = $this->select_word($id_user, $id_word, $bdd);
+        $failures = $data_word['failures'] + 1;
+        $query = $bdd->prepare("UPDATE voc_already_read SET failures = :failures WHERE id_word = :id_word AND id_user = :id_user");
+        $query->execute(array(":id_word" => $id_word, ":id_user" => $id_user, ":failures" => $failures));
+    }
+
+    private function add_success($id_user, $id_word, $bdd) {
+        $data_word = $this->select_word($id_user, $id_word, $bdd);
+        $success = $data_word['success'] + 1;
+        $query = $bdd->prepare("UPDATE voc_already_read SET success = :success WHERE id_word = :id_word AND id_user = :id_user");
+        $query->execute(array(":id_word" => $id_word, ":id_user" => $id_user, ":success" => $success));
+    }
 }
 
 ?>
